f1dfd612fdb536d894e29ccc6d06f56bd528e388
Mayank Mittal <mayankmittal3009@gmail.com>
2025-06-26 23:03:07 +0530

    💄 Add responsive layout for cards, filters, and mobile views

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req,res)=>{
             if(err){
                 return next(err);
             }
-            req.flash("success","welcome to Homyfy");
+            req.flash("success","welcome to WanderLust!");
             res.redirect("/listings");
         })
     }catch(e){
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req,res)=>{
 };
 
 module.exports.login = async(req,res)=>{
-    req.flash("success","Welcome back to Homyfy!");
+    req.flash("success","Welcome back to WanderLust!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 }
